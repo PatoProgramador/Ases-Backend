5753df8d2cbe2ab6a1c41349c6bd5fabcb907d3b
PatoProgramador <skpates18@gmail.com>
2026-03-29 18:23:15 -0500

    First commit

---
+++ src/test/java/com/ases/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ases.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
